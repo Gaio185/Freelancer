ced23f39234c5ad391159ba7720c3ed14a7cbad1
doutorGaspar <120458931+doutorGaspar@users.noreply.github.com>
2024-10-08 19:02:11 +0100

    Mesa e Vault fixed

--- Assets/Scripts/Player Controller Script/Vaulting.cs
+++ Assets/Scripts/Player Controller Script/Vaulting.cs
@@ -4,38 +4,48 @@ using UnityEngine;
 
 public class Vaulting : MonoBehaviour
 {
-    // Start is called before the first frame update
     private int vaultLayer;
     public Camera cam;
     private float playerHeight = 2f;
     private float playerRadius = 0.5f;
+
     void Start()
     {
-        vaultLayer = LayerMask.NameToLayer("VaultLayer");
-        vaultLayer = ~vaultLayer;
+        vaultLayer = 1 << LayerMask.NameToLayer("VaultLayer");  // Fix layer mask setup
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vault();
     }
+
     private void Vault()
     {
-        if (Input.GetKey(KeyCode.Space))
+        float cameraAngle = Vector3.Dot(cam.transform.forward, Vector3.up);
+
+        // Prevent vaulting if the player is looking too far down or too far up
+        if (cameraAngle < 0.8f && cameraAngle > -0.8f)
         {
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out var firstHit, 1f, vaultLayer))
+            if (Input.GetKey(KeyCode.Space))
             {
-                print("vaultable in front");
-                if (Physics.Raycast(firstHit.point + (cam.transform.forward * playerRadius) + (Vector3.up * 0.6f * playerHeight), Vector3.down, out var secondHit, playerHeight))
+                // Adjust the ray origin slightly higher to avoid hitting unwanted objects when looking down
+                Vector3 rayOrigin = transform.position + Vector3.up * (0.5f * playerHeight);
+
+                if (Physics.Raycast(rayOrigin, cam.transform.forward, out var firstHit, 0.8f, vaultLayer))
                 {
-                    print("found place to land");
-                    StartCoroutine(LerpVault(secondHit.point, 0.5f));
+                    print("vaultable in front");
+
+                    // Cast the second ray to find a landing position
+                    if (Physics.Raycast(firstHit.point + (cam.transform.forward * playerRadius) + (Vector3.up * 0.6f * playerHeight), Vector3.down, out var secondHit, playerHeight))
+                    {
+                        print("found place to land");
+                        StartCoroutine(LerpVault(secondHit.point, 0.5f));
+                    }
                 }
             }
         }
-
     }
+
     IEnumerator LerpVault(Vector3 targetPosition, float duration)
     {
         float time = 0;
