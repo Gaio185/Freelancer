04c660dc0b7d89eac621697da0b0d2fafd932f0d
Gaio185 <a25962@alunos.ipca.pt>
2024-10-19 22:47:11 +0100

    state machine working with chasing

--- Assets/Scripts/AI/AIChasePlayerState.cs
+++ Assets/Scripts/AI/AIChasePlayerState.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIChasePlayerState : AiState
 {
-    public void Enter(AiAgent agent)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void Exit(AiAgent agent)
-    {
-        throw new System.NotImplementedException();
-    }
+    public Transform playerTransform;
 
     public AiStateId GetId()
     {
-        throw new System.NotImplementedException();
+        return AiStateId.Chase;
+    }
+
+    public void Enter(AiAgent agent)
+    {
+        if(playerTransform == null)
+        {
+            playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+
     }
 
     public void Update(AiAgent agent)
     {
-        throw new System.NotImplementedException();
+        if (!agent.navMeshAgent.enabled) { return; }
+
+        if (!agent.navMeshAgent.hasPath)
+        {
+            agent.navMeshAgent.destination = playerTransform.position;
+        }
+
+        Vector3 direction = playerTransform.position - agent.navMeshAgent.destination;
+        direction.y = 0;
+
+        if (direction.sqrMagnitude > agent.config.maxDistance * agent.config.maxDistance)
+        {
+            if (agent.navMeshAgent.pathStatus != NavMeshPathStatus.PathPartial)
+            {
+                agent.navMeshAgent.destination = playerTransform.position;
+            }
+
+        }
     }
+
+    public void Exit(AiAgent agent)
+    {
+
+    }
+
+    
+
+    
 }
--- Assets/Scripts/AI/AiAgent.cs
+++ Assets/Scripts/AI/AiAgent.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiAgent : MonoBehaviour
 {
     public AIStateMachine stateMachine;
     public AiStateId initialState;
+    public NavMeshAgent navMeshAgent;
+    public AiAgentConfig config;
 
     // Start is called before the first frame update
     void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();    
         stateMachine = new AIStateMachine(this);
+        stateMachine.RegisterStates(new AIChasePlayerState());
         stateMachine.ChangeState(initialState);
     }
 
