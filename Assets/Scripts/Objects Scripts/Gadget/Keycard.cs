3aedf39c4447b240f390f7994ceab1e1cec4fbf7
Gaio185 <a25962@alunos.ipca.pt>
2024-11-18 04:02:04 +0000

    keycard rework

--- Assets/Scripts/Objects Scripts/Gadget/Keycard.cs
+++ Assets/Scripts/Objects Scripts/Gadget/Keycard.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public enum DivisionType
 {
-    DivisionA,
-    DivisionB,
-    DivisionC,
-    DivisionD,
-    DivisionE
+    Worker,
+    Executive,
+    IT,
+    CEO,
+    Security
 }
 
 public class Keycard : MonoBehaviour
 {
     public DivisionType divisionType; // Set this in the Unity Editor to DivisionA, DivisionB, or DivisionC
+    private bool isInRange;
+    private GameObject player;
 
     public DivisionType GetDivisionType()
     {
         return divisionType;
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        isInRange = false;
+    }
+
+    private void Update() { 
+        if(isInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            player.GetComponent<Player>().keycards.Add(this);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
     }
 }
