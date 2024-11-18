2699affb9128f2dd96f4b741c39c2806b295d993
Gaio185 <a25962@alunos.ipca.pt>
2024-11-18 00:15:23 +0000

    terminal colliders and collider bug fixing

--- Assets/Scripts/Extraction/ExtractionZone.cs
+++ Assets/Scripts/Extraction/ExtractionZone.cs
@@ -19,4 +19,12 @@ public class ExtractionZone : MonoBehaviour
             isInRange = true;
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
 }
---
+++ Assets/Scripts/UIScripts/TerminalCollisionCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalCollisionCheck : MonoBehaviour
+{
+    [HideInInspector] public bool isInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInRange = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+}
