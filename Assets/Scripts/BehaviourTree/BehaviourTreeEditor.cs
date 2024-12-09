b8fad12e22c1484681c4f0432aad0afb2abe13a1
João Pereira <jotamigl@gmail.com>
2024-12-09 04:13:09 +0000

    Interface das BTs implementada

---
+++ Assets/Scripts/BehaviourTree/BehaviourTreeEditor.cs
@@ -0,0 +1,53 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BehaviourTreeEditor : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    BehaviourTreeView treeView;
+    InspectorView inspectorView;
+
+    [MenuItem("BehaviourTreeEditor/Editor ...")]
+    public static void OpenWindow()
+    {
+        BehaviourTreeEditor wnd = GetWindow<BehaviourTreeEditor>();
+        wnd.titleContent = new GUIContent("BehaviourTreeEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // Instantiate UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/BehaviourTree/BehaviourTreeEditor.uxml");
+        visualTree.CloneTree(root);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/BehaviourTree/BehaviourTreeEditor.uss");
+        root.styleSheets.Add(styleSheet);
+
+        treeView = root.Q<BehaviourTreeView>();
+        inspectorView = root.Q<InspectorView>();
+        treeView.OnNodeSelected = OnNodeSelectionChanged;
+        OnSelectionChange();
+    }
+
+    private void OnSelectionChange()
+    {
+        BehaviourTree tree = Selection.activeObject as BehaviourTree;
+        if (tree && AssetDatabase.CanOpenAssetInEditor(tree.GetInstanceID()))
+        {
+            treeView.PopulateView(tree);
+        }
+    }
+
+    void OnNodeSelectionChanged(NodeView node)
+    {
+        inspectorView.UpdateSelection(node);
+    }
+}
