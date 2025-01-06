fe4c271eb0495000321fdbcf31571950253d1303
Gaio185 <a25962@alunos.ipca.pt>
2025-01-06 08:04:59 +0000

    BETA

--- Assets/Scripts/UIScripts/TerminalInteract.cs
+++ Assets/Scripts/UIScripts/TerminalInteract.cs
@@ -31,29 +31,40 @@ public class TerminalInteract : MonoBehaviour
             TerminalManagement terminalManagement = computerInterface.GetComponent<TerminalManagement>();
 
             terminalCollisionCheck.player.interactPanel.SetActive(true); // Show the interact panel
-            if (Input.GetKeyDown(KeyCode.F) && !overridePenDrive.enabled)
+            if (Input.GetKeyDown(KeyCode.F))
             {
-                playerScript.canPause = false; // Disable pause
-                playerScript.HUD.SetActive(false); // Hide the HUD
-                computerInterface.SetActive(true); // Show the terminal UI
-                Cursor.visible = true; // Show cursor
-                Cursor.lockState = CursorLockMode.None; // Unlock cursor
-                playerScript.movement.canMove = false; // Disable player movement
-                playerScript.switchWeapon.disableTools = true; // Disable player tools
-                playerScript.switchWeapon.DeactivateAllModels();
-                computerInterface.GetComponent<TerminalManagement>().isUnlocked = true;
-            }
-            
-
-            if (Input.GetKeyDown(KeyCode.F) && overridePenDrive.enabled)
-            {
-                TerminalManagement terminal = computerInterface.GetComponent<TerminalManagement>();
-                if (!terminal.isUnlocked && terminal.canBeHacked)
+                if(overridePenDrive != null)
                 {
-                    --overridePenDrive.useCount;
-                    overridePenDrive.penUI[overridePenDrive.useCount].SetActive(false);
-                    terminal.passwordInterface.SetActive(false); // Hide the password interface
-                    terminal.workspaceInterface.SetActive(true); // Show the workspace interface
+                    if (overridePenDrive.isActiveAndEnabled)
+                    {
+                        TerminalManagement terminal = computerInterface.GetComponent<TerminalManagement>();
+                        if (!terminal.isUnlocked && terminal.canBeHacked)
+                        {
+                            USBPenOverride usb = overridePenDrive.GetComponent<USBPenOverride>();
+                            --usb.useCount;
+                            usb.penUI[usb.useCount].SetActive(false);
+                            terminal.passwordInterface.SetActive(false); // Hide the password interface
+                            terminal.workspaceInterface.SetActive(true); // Show the workspace interface
+                            playerScript.canPause = false; // Disable pause
+                            playerScript.HUD.SetActive(false); // Hide the HUD
+                            computerInterface.SetActive(true); // Show the terminal UI
+                            Cursor.visible = true; // Show cursor
+                            Cursor.lockState = CursorLockMode.None; // Unlock cursor
+                            playerScript.movement.canMove = false; // Disable player movement
+                            playerScript.switchWeapon.disableTools = true; // Disable player tools
+                            playerScript.switchWeapon.DeactivateAllModels();
+                            audioSourceVerify.PlayOneShot(audioSourceVerify.clip); // Play sound for successful login
+                            terminal.isUnlocked = true;
+                            Debug.Log("Terminal bypassed with USB Pen Drive.");
+                        }
+                        else if (!terminal.isUnlocked)
+                        {
+                            audioSourceDeny.PlayOneShot(audioSourceDeny.clip);
+                        }
+                    }
+                }
+                else if(overridePenDrive == null)
+                {
                     playerScript.canPause = false; // Disable pause
                     playerScript.HUD.SetActive(false); // Hide the HUD
                     computerInterface.SetActive(true); // Show the terminal UI
@@ -62,13 +73,7 @@ public class TerminalInteract : MonoBehaviour
                     playerScript.movement.canMove = false; // Disable player movement
                     playerScript.switchWeapon.disableTools = true; // Disable player tools
                     playerScript.switchWeapon.DeactivateAllModels();
-                    audioSourceVerify.PlayOneShot(audioSourceVerify.clip); // Play sound for successful login
-                    terminal.isUnlocked = true;
-                    Debug.Log("Terminal bypassed with USB Pen Drive.");
-                }
-                else if(!terminal.isUnlocked)
-                {
-                    audioSourceDeny.PlayOneShot(audioSourceDeny.clip); 
+                    computerInterface.GetComponent<TerminalManagement>().isUnlocked = true;
                 }
             }
         }
