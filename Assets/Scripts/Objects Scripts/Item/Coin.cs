e231350ec98b2a6e24d75db96931bf0ae4fff56a
Gaio185 <a25962@alunos.ipca.pt>
2025-01-13 01:02:33 +0000

    interaction panel changes

--- Assets/Scripts/Objects Scripts/Item/Coin.cs
+++ Assets/Scripts/Objects Scripts/Item/Coin.cs
@@ -11,10 +11,13 @@ public class Coin : MonoBehaviour
     private GameObject launchPoint;
     private CoinLauncher coinLauncher;
 
+    private Player player;
+
     void Start()
     {
         launchPoint = GameObject.FindGameObjectWithTag("CoinSpawn");
         coinLauncher = launchPoint.GetComponent<CoinLauncher>();
+        player = FindObjectOfType<Player>();
     }
 
     // Update is called once per frame
@@ -22,10 +25,20 @@ public class Coin : MonoBehaviour
     {
         Collider[] rangeChecks = Physics.OverlapSphere(transform.position, pickUpRange, targetMask);
 
-        if (rangeChecks.Length != 0 && Input.GetKeyDown(KeyCode.F) && coinLauncher.coinCount < coinLauncher.coinList.Count)
+        if (rangeChecks.Length != 0)
         {
-            Destroy(this.gameObject);
-            coinLauncher.PickUpCoinUI();
+            player.interactionText.text = "Press F to Pick Up";
+            player.interactPanel.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.F) && coinLauncher.coinCount < coinLauncher.coinList.Count)
+            {
+                Destroy(this.gameObject);
+                player.interactPanel.SetActive(false);
+                coinLauncher.PickUpCoinUI();
+            }
+        }
+        else
+        {
+            player.interactPanel.SetActive(false);
         }
     }
 }
